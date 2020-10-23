336192ea5cb5d79929898e47ef9823d0b736de2c
Shreyas Ashtamkar <shreyu@programmer.net>
2020-10-23 14:06:14 +0530

    Wireframe Ready

--- runner.py
+++ runner.py
@@ -1,1 +1,57 @@
- 
+'''
+#TODO: Start
+#TODO: Store PID in runner.pid
+#TODO: Read Config File
+#TODO: Check for errors
+#TODO: Build a list of "run" records that specifies a time and program to run.
+#TODO: Define up the function to catch the USR1 signal and print run records
+#TODO: Sort run records by time
+#TODO: Start Background Process
+#TODO: Take the next record off the list and wait for that time, then run the program
+#TODO: Add a record to the "result" list
+#TODO: If this was an "every" record", add an adjusted record to the "run" list
+#TODO: Repeat until no more to records on the "run" list
+#TODO: Exit
+'''
+#!/usr/bin/env python3
+# -*- coding: ascii -*-
+
+import sys
+import os
+import time
+import datetime
+import signal
+
+class Command:
+    def __init__(self, timespec, programPath, parameters):
+        timespec    = timespec
+        programPath = programPath
+        parameters  = parameters
+
+runRecords = []
+
+if __name__ == "__main__":
+
+    #Write PID to File
+    with open("runner.pid", 'w') as pidfile:
+        pidfile.write(str(os.getpid()))
+    
+    #Read Config Files
+    with open("runner.conf") as configFile:
+        line = configFile.readline().strip().split()
+
+        while len(line) > 0:
+            run = line.index('run')
+            if line[run+1:]:
+                params = ''
+            else:
+                params = ' '.join(line[run+1:])
+            
+            runRecords.append(
+                Command(
+                    timespec    = line[0:run],
+                    programPath = line[run],
+                    parameters  = params
+                )
+            )
+            line = configFile.readline().strip().split()
--- runstatus.py
+++ runstatus.py
@@ -1,1 +1,21 @@
- 
+"""
+#TODO: Start
+#TODO: Read PID from runner.pid
+#TODO: Give an error message if file not found or bad pid
+#TODO: Send USR1 Signal to runner.py (Through PID)
+#TODO: Read runner.status
+#TODO: Give output
+#TODO: Open runner.status
+#TODO: If Errors Retry for 5 seconds. 
+#TODO: Truncate the File to zero length
+#TODO: Close File
+#TODO: Stop
+"""
+#!/usr/bin/env python3
+# -*- coding: ascii -*-
+
+import sys
+import os
+
+pidfile = ".runner-pid"
+statusfilename = ".runner.status"
